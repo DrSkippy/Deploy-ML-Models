ee31f1a6da3e6f222e8b72ae69602a872fbf3544
DrSkippy <scott@aspenmesh.io>
2022-06-01 10:06:41 -0600

    presentation skeloton in worksheet

--- SimpleFlaskDeploy/model/training_data.py
+++ SimpleFlaskDeploy/model/training_data.py
@@ -1,5 +1,9 @@
+import pandas as pd
+import numpy as np
+
 file_path = "../data/adult_earning/"
 hfile = file_path + "adult.names"
+dfile = file_path + "adult.data"
 
 def get_header():
     header = []
@@ -8,6 +12,11 @@ def get_header():
             if line.startswith("|") or line.startswith(">") or line.startswith("\n"):
                 continue
             else:
-                header.append(line.split(":")[-1])
+                header.append(line.split(":")[0])
     header.append("class")
     return header
+
+def get_training_dataframe():
+    header = get_header()
+    df = pd.read_csv(dfile, names=header)
+    return df
