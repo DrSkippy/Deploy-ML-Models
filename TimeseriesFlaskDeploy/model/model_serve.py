282a733b1dff4a014cc59869ad4956f7fcaff28f
DrSkippy <scott@aspenmesh.io>
2022-06-07 15:50:39 -0600

    First attempt to deploy

--- TimeseriesFlaskDeploy/model/model_serve.py
+++ TimeseriesFlaskDeploy/model/model_serve.py
@@ -1,6 +1,6 @@
 __version__ = '0.1.0'
 
-from flask import Flask, Response, request, send_file
+from flask import Flask, Response, request
 import json
 import datetime
 
@@ -8,8 +8,12 @@ import numpy as np
 import pandas as pd
 
 from joblib import dump, load
-from prophet import Prophet
 
+from prophet import Prophet
+from prophet.diagnostics import performance_metrics
+from prophet.diagnostics import cross_validation
+
+# data management utilities
 from training_data import *
 
 # server configuration
@@ -31,19 +35,6 @@ dictConfig({
 })
 
 app = Flask(__name__)
-
-
-class NumpyArrayEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        else:
-            return super(NumpyArrayEncoder, self).default(obj)
-
 
 @app.route('/version')
 def version():
@@ -85,27 +76,17 @@ def predict():
     :return:
     """
     records = request.get_json()
-    res = records["data"]
+    res = np.array(records["data"])
     periods = int(records["size"])
-    m = load("../data/model.pkl")
+    m = load(file_path + "model.pkl")
     if len(res) == 0:
         future = m.make_future_dataframe(periods=periods)
     forecast = m.predict(future)
-    res = forecast.to_numpy()
-    header = forecast.columns
-    rdata = {
-        "size": res.shape,
-        "data": res,
-        "header": header
-    }
-    if len(res["errors"]) > 0:
-        rdata = json.dumps(res, cls=NumpyArrayEncoder)
-    else:
-        df = pd.DataFrame(records["data"], columns=training_data.features)
-        model_nn_sc = load("../data/neural_net_scaler.pkl")
-        data = model_nn_sc.transform(df)
-        y_pred = model_nn.predict(data)
-        rdata = json.dumps({"size": len(y_pred), "data": [np.argmin(x) for x in y_pred]}, cls=NumpyArrayEncoder)
+    rdata = json.dumps({
+        "size": forecast.shape,
+        "data": forecast.to_numpy(),
+        "header": forecast.columns
+    }, cls=NumpyArrayEncoder)
     response_headers = [
         ('Content-type', 'application/json'),
         ('Content-Length', str(len(rdata)))
@@ -114,7 +95,7 @@ def predict():
 
 
 @app.route('/train', methods=['POST'])
-def examples():
+def train():
     """
     JSON Post Payload:
     { "size": 4,
@@ -127,17 +108,18 @@ def examples():
     :return:
     """
     records = request.get_json()
-    periods = int(records["size"])
+    periods = int(records["size"][0])
+    vector_length = int(records["size"][1])
+    assert (vector_length == 2)
     data = np.array(records["data"])
     df = pd.DataFrame(data, columns=['ds', 'y'])
     m = Prophet()
     m.fit(df)
-    m = dump("../data/model.pkl")
-    res = {
-        "size": n,
-        "data": data
-    }
-    rdata = json.dumps(res, cls=NumpyArrayEncoder)
+    dump(m, filename=file_path + "model.pkl")
+    # evaluation
+    df_cv = cross_validation(m, initial='730 days', period='180 days', horizon='365 days')
+    df_p = performance_metrics(df_cv)
+    rdata = df_p.to_json()
     response_headers = [
         ('Content-type', 'application/json'),
         ('Content-Length', str(len(rdata)))
--- TimeseriesFlaskDeploy/model/training_data.py
+++ TimeseriesFlaskDeploy/model/training_data.py
@@ -1,9 +1,11 @@
 import csv
+import json
 
 import pandas as pd
 import numpy as np
 
 file_path = "../data/"
+#file_path = "./data/"
 dfile = file_path + "example_wp_log_peyton_manning.csv"
 
 
@@ -20,3 +22,19 @@ def get_training_data():
                 data.append(r)
         data = np.array(data)
         return data, header
+
+
+class NumpyArrayEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, pd.Timestamp):
+            return str(obj)
+        elif isinstance(obj, pd.Index):
+            return list(obj)
+        elif isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NumpyArrayEncoder, self).default(obj)
